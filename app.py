94ede8bb63266eebd77ddba8472f282087ae2de6
JoelRodriguez-dev <joel.rodriguez.soto@estudiante.ipss.cl>
2025-08-17 23:29:25 +0000

    sin Dockfile

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask (--name--)
+
+@add.route('/')
+def inicio():
+
+	return "Aplicando en CI/CD // jenkins y docker"
+if --name--=='--main--':
+	app.run (host="0.0.0.0", port 8888)
